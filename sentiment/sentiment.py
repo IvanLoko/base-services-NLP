50e6058f71b77448fb605384db715ff4f06a438e
IvanLoko <100134925+IvanLoko@users.noreply.github.com>
2024-11-25 15:40:21 +0300

    Update sentiment.py

--- sentiment/sentiment.py
+++ sentiment/sentiment.py
@@ -1,36 +1,39 @@
-import json
-
-import psycopg2
 from transformers import pipeline
 from fastapi import FastAPI, File
 import uvicorn
+from typing import List
+import argparse
+
 
 app = FastAPI()
 
-
-
 @app.post('/sentiment')
-def sentiment_inference(file: bytes = File(...)):
+def sentiment_inference(file: List[str]):
 
-    inp = json.loads(file.decode('utf-8'))
+    inp = file
 
-    output = model.predict(inp['text'])
+    output = model(inp, **model_kwargs)
 
     for num, value in enumerate(output):
-        value['text'] = inp['text'][num]
+        value['text'] = inp[num]
 
-    for val in output:
-
-        cursor.execute("INSERT INTO main (request_id, task, input_text, sentiment) VALUES (%s, %s, %s, %s)",
-                       ('request_id', 'sentiment', val['text'], val['label']))
-    conn.commit()
+    #Task.current_task().upload_artifact(
+    #    name=f'temp {datetime.now().strftime("%Y-%m-%d-%H:%M:%S")}',
+    #    artifact_object=output,
+    #)
 
     return output
 
-if __name__ == '__main__':
-    model = pipeline('sentiment-analysis', model='seara/rubert-base-cased-russian-sentiment', device='cuda')
 
-    conn = psycopg2.connect(dbname="admindb", user="postgres", password="3115", host="127.0.0.1")
-    cursor = conn.cursor()
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--device', type=str, default='cuda')
+    parser.add_argument('--score', type=float, default=0.6)
+
+    args = parser.parse_args()
+
+    model_kwargs = {'max_length': 512, 'truncation': True}
+    model = pipeline('sentiment-analysis', model='seara/rubert-base-cased-russian-sentiment', device=args.device)
 
     uvicorn.run(app, host='0.0.0.0', port=5555)
